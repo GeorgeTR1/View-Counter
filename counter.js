4815c7b8cbe58ddb118748da32036d72f509477d
GeorgeTR1 <35677090+GeorgeTR1@users.noreply.github.com>
2025-02-27 16:06:20 -0600

    Create counter.js

---
+++ counter.js
@@ -0,0 +1,4 @@
+var req = new XMLHttpRequest();
+//URL is deployment of code here: https://github.com/GeorgeTR1/View-Counter/blob/main/main.js
+req.open("POST", "https://georgetr1-view-counte-39.deno.dev/");
+req.send(location.href);
