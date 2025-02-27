67632c4cbbd1bd0a1c70ea5fb9c0e4956d43de96
GeorgeTR1 <35677090+GeorgeTR1@users.noreply.github.com>
2025-02-27 17:05:43 -0600

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,7 +1,4 @@
-console.log("hello, world");
-/*
 var req = new XMLHttpRequest();
 //URL is deployment of code here: https://github.com/GeorgeTR1/View-Counter/blob/main/main.js
 req.open("POST", "https://georgetr1-view-counte-39.deno.dev/");
 req.send(location.href);
-*/
